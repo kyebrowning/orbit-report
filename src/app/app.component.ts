c340934314f1fdb667763c6765ee7d5fa5787822
Kye-Browning <kye.browning@gmail.com>
2020-11-18 17:38:02 -0600

    added search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,11 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   
 
   constructor() {
+    this.displayList = []
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
@@ -24,7 +26,24 @@ export class AppComponent {
           this.sourceList.push(satellite);
         };
 
+        // make a copy of the sourceList to be shown to the user
+        this.displayList = this.sourceList.slice(0);
+
       }.bind(this));
    }.bind(this));
  }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
